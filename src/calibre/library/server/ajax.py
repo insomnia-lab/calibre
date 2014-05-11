ac7c0045260817c1bee73b0b7487c8be7393cf2b
ael-code <tommy.ael@gmail.com>
2014-05-12 01:42:23 +0200

    string changed

--- src/calibre/library/server/ajax.py
+++ src/calibre/library/server/ajax.py
@@ -198,18 +198,7 @@ class AjaxServer(object):
 
     @Endpoint(set_last_modified=False)
     def ajax_page_book_add(self):
-        return """
-        <!DOCTYPE html>
-         <html><body>
-            <h2>Upload a file</h2>
-            <form action="upload" method="post" enctype="multipart/form-data">
-            filename: <input type="file" name="myFile" /><br />
-            <input type="submit" />
-            </form>
-            <h2>Download a file</h2>
-            <a href='download'>This one</a>
-        </body></html>
-        """
+        return """<!DOCTYPE html><html><body><h2>Upload a file</h2><form action="upload" method="post" enctype="multipart/form-data"> filename: <input type="file" name="myFile" /><br /><input type="submit" /></form><h2>Download a file</h2><a href='download'>This one</a></body></html>"""
         index.exposed = True
     
     
