30bf4f7e8ebe56686ad8f6e68dad8555d5b61a6c
ael-code <tommy.ael@gmail.com>
2014-05-20 17:39:05 +0200

    Cleaned-up Upload module Remove reinitialization of Database object Remove unused import Cleaned-up endpoint decorator class

--- src/calibre/library/server/upload.py
+++ src/calibre/library/server/upload.py
@@ -5,27 +5,11 @@ __license__   = 'GPL v3'
 __copyright__ = '2010, Kovid Goyal <kovid@kovidgoyal.net>'
 __docformat__ = 'restructuredtext en'
 
-import operator, os, re, time
+import os
 import cherrypy
-
-#propirata import
 import tempfile
-from calibre.db.legacy import LibraryDatabase
 
-from binascii import hexlify, unhexlify
-from collections import OrderedDict
-from calibre.constants import filesystem_encoding, config_dir
-from calibre import (isbytestring, force_unicode, fit_image, prepare_string_for_xml, sanitize_file_name2)
-from calibre.utils.filenames import ascii_filename
-from calibre.utils.icu import sort_key
-from calibre.utils.magick import Image
-from calibre.library.comments import comments_to_html
-from calibre.library.server import custom_fields_to_display
-from calibre.library.field_metadata import category_icon_map
-from calibre.library.server.utils import quote, unquote
-from calibre.db.categories import Tag
-from calibre.ebooks.metadata.sources.identify import urls_from_identifiers
-from calibre.library.cli import command_add, do_add
+from calibre.library.cli import do_add
 
 def utf8(x):  # {{{
     if isinstance(x, unicode):
@@ -34,34 +18,20 @@ def utf8(x):  # {{{
 # }}}
 
 class Endpoint(object):  # {{{
-    'Manage encoding, mime-type, last modified, cookies, etc.'
-    def __init__(self, mimetype='text/html; charset=utf-8', sort_type='category'):
+    'Manage encoding, mime-type '
+    def __init__(self, mimetype='text/html; charset=utf-8'):
         self.mimetype = mimetype
-        self.sort_type = sort_type
-        self.sort_kwarg = sort_type + '_sort'
-        self.sort_cookie_name = 'calibre_browse_server_sort_'+self.sort_type
+       
     def __call__(eself, func):
         def do(self, *args, **kwargs):
-#            if 'json' not in eself.mimetype:
-#                sort_val = None
-#                cookie = cherrypy.request.cookie
-#                if eself.sort_cookie_name in cookie:
-#                    sort_val = cookie[eself.sort_cookie_name].value
-#                kwargs[eself.sort_kwarg] = sort_val
-#
-#            # Remove AJAX caching disabling jquery workaround arg
-#            kwargs.pop('_', None)
             ans = func(self, *args, **kwargs)
             cherrypy.response.headers['Content-Type'] = eself.mimetype
-            updated = self.db.last_modified()
-            cherrypy.response.headers['Last-Modified'] = \
-                self.last_modified(max(updated, self.build_time))
             ans = utf8(ans)
             return ans
         do.__name__ = func.__name__
         return do
 # }}}
-
+ 
 class UploadServer(object):
         
     def add_routes(self, connect):
@@ -132,13 +102,8 @@ class UploadServer(object):
         #   direct way   
         #   using do_add
         #   do_add(db, paths, one_book_per_directory, recurse, add_duplicates, otitle, oauthors, oisbn, otags, oseries, oseries_index, ocover)
-        db = get_db(self.opts.with_library)
+        
+        db = self.db
         do_add(db,[tmpFile[1]],False,False,False,book_file.filename,[book_author],None,["web_uploaded"],book_isbn,None,None)
         return out % (size, book_file.filename, book_file.content_type,book_file.filename,[book_author])
         upload.exposed = True
-        
-def get_db(library_path):
-    dbpath = os.path.expanduser(library_path)
-    dbpath = os.path.abspath(dbpath)
-    return LibraryDatabase(dbpath)
-    
